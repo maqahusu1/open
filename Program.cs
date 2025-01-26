1516020beee7d5970abc44059df656e12f63c7cf
maqahusu1 <maqaahhsu@gmail.com>
2025-01-26 22:36:15 +0400

    first

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.Write("Enter Weather:");
 
+
+string weatherStatus = Console.ReadLine();
+
+if (weatherStatus == "gunesli")
+    Console.WriteLine("Eynek tax");
+else if (weatherStatus == "yagisli")
+    Console.WriteLine("zontik gotur");
+else if (weatherStatus == "soyuq")
+    Console.WriteLine("qalin gey");
+else
+    Console.WriteLine("duzgun qeyd etmemisiniz");
